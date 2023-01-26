b905691a0e198a5d3f01d20620fa5e27fa6e2a76
Efrain Luciano Fernandez Valverde <efrain19091@gmail.com>
2023-01-26 05:14:46 +0000

    Add ckeck tie function

--- assets/js/script.js
+++ assets/js/script.js
@@ -26,7 +26,10 @@ function startGame() {
 }
 
 function turnClick(square) {
+   if (typeof origBoard[square.target.id] == 'number') {
     turn(square.target.id, huPlayer)
+    if (!checkTie()) turn(bestSpot(), aiPlayer);
+   }     
 }
 
 function turn(squareId, player) {
@@ -57,4 +60,24 @@ function gameOver(gameWon)  {
     for (let i = 0; i < cells.length; i++) {
         cells[i].removeEventListener('click', turnClick, false)
     }
+}
+
+function emptySquares() {
+    return origBoard.filter(s => typeof s == 'number');
+}
+
+function bestSpot() {
+    return emptySquares()[0];
+}
+
+function checkTie() {
+    if (emptySquares().length == 0) {
+        for (var i = 0; i < cells.length; i++) {
+            cells[i].style.backgroundColor = "green";
+            cells[i].removeEventListener('click', turnClick, false);
+        }
+        declareWinner("Tie Game")
+        return true;
+    }
+   return false;
 }
